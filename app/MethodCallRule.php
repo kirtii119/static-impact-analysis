d301f512bfff3d557f477c33882acc58953c4ced
Kirti Agarwal <kirtiagarwal119@gmail.com>
2024-02-27 14:56:45 +0530

    json formatted

--- app/MethodCallRule.php
+++ app/MethodCallRule.php
@@ -18,15 +18,26 @@ class MethodCallRule implements \PHPStan\Rules\Rule
 
 	public function processNode(Node $node, Scope $scope): array
 	{
-		$resultFile = './result2.txt';
+		$resultFile = './result.json';
         $meraData = $node->get(MethodCallCollector::class);
+		
 
 		foreach ($meraData as $file => $declarations) {
-			file_put_contents($resultFile, $file . PHP_EOL , FILE_APPEND);
+			$result = [];
+			// file_put_contents($resultFile, $file . PHP_EOL , FILE_APPEND);
 			foreach ($declarations as [$name, $line]) {
-				file_put_contents($resultFile, "$name - $line" . PHP_EOL , FILE_APPEND);
-				
+				foreach ($name as $myKey => $myValue){
+					if(array_key_exists($myKey, $result)){
+						$result[$myKey][] = $myValue;
+					}
+					else{
+						$result[$myKey] = [$myValue];
+					}
+				}
 			}
+			// print_r($result);
+			
+			file_put_contents($resultFile, json_encode($result) . PHP_EOL , FILE_APPEND);
 		}
 
 
