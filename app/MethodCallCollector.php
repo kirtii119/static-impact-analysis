120c70f97b01d3df86651dcb5179b3b1fbb4a8e6
SYBhavsar <bhavsarsuyash30@gmail.com>
2024-02-29 12:49:20 +0530

    modified method collector

--- app/MethodCallCollector.php
+++ app/MethodCallCollector.php
@@ -23,19 +23,20 @@ class MethodCallCollector implements Collector
         $methCall = $node->name->name; //will only work if name is an identifier, it can also be an expression
         $resolvedType = $scope->getType($node->var); //gives type of method caller
         $funcName = $scope->getFunctionName();
-        
-        if ($scope->isInClass()){
+
+        if ($scope->isInClass()) {
             $orgClassName = $scope->getClassReflection()->getName();
-            $funcName = $orgClassName."::".$funcName;
+            $funcName = $orgClassName . "::" . $funcName;
         }
         // if($resolvedType instanceof \PHPStan\Type\ObjectType or $resolvedType instanceof \PHPStan\Type\ThisType  ){
         //     $methCallClassName =  $resolvedType->getClassName(); //getClassName works only for object types
         // }
+
         if ($resolvedType instanceof \PHPStan\Type\ObjectType or $resolvedType instanceof \PHPStan\Type\ThisType) {
             $methCallClassName = $resolvedType->getClassName(); //getClassName works only for object types
-        } elseif($resolvedType instanceof \PHPStan\Type\MixedType) {
-            $methCallClassName = "Mixedty"; //getClassName works only for object types
-        } elseif($resolvedType instanceof \PHPStan\Type\IntersectionType or $resolvedType instanceof \PHPStan\Type\UnionType ) {
+        } elseif ($resolvedType instanceof \PHPStan\Type\MixedType) {
+            $methCallClassName = "Mixedty";            //getClassName works only for object types
+        } elseif ($resolvedType instanceof \PHPStan\Type\IntersectionType or $resolvedType instanceof \PHPStan\Type\UnionType) {
             $methCallClassName = $resolvedType->toPhpDocNode();//this gives list of possible types 
         } elseif ($resolvedType instanceof \PHPStan\Type\StringType) {
             $methCallClassName = "str"; //getClassName works only for object types
@@ -47,12 +48,18 @@ class MethodCallCollector implements Collector
             $methCallClassName = "resourcety"; //getClassName works only for object types
         } elseif ($resolvedType instanceof \PHPStan\Type\ArrayType) {
             $methCallClassName = "arraty"; //getClassName works only for object types
-        } else {
+        } elseif ($resolvedType instanceof \PHPStan\Type\NullType) {
+            $methCallClassName = "nullty"; //getClassName works only for object types
+        } elseif ($resolvedType instanceof \PHPStan\Type\IntegerType) {
+            $methCallClassName = "intty"; //getClassName works only for object types
+        } elseif ($resolvedType instanceof \PHPStan\Type\FloatType) {
+            $methCallClassName = "floatty"; //getClassName works only for object types
+        }else {
             $methCallClassName = $resolvedType->getClassName();
         }
 
-        $methCall = $methCallClassName."::".$methCall;
-        
+        $methCall = $methCallClassName . "::" . $methCall;
+
         return [array($funcName => $methCall), $node->getLine()];
 
     }
