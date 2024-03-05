9086430dcf85e22e2392a44bc54174470c2727c3
Kirti Agarwal <kirtiagarwal119@gmail.com>
2024-03-05 13:01:42 +0530

    modified union test case

--- tests/testCases/UnionTestCase.php
+++ tests/testCases/UnionTestCase.php
@@ -1,21 +1,55 @@
 <?php
-class Foo {
-    /**
-     * @param Foo[]|Collection $object
-     */
-    public function doSomethingUseful($object)
-    {
-        if ($object instanceof Foo) {
-            // now we can be sure that $object is just Foo in this branch
-        } elseif ($object instanceof Bar) {
-            // dtto for Bar
-        }
+// class Foo {
+//     /**
+//      * @param Foo[]|Collection $object
+//      */
+//     public function doSomethingUseful($object)
+//     {
+//         if ($object instanceof Foo) {
+//             // now we can be sure that $object is just Foo in this branch
+//         } elseif ($object instanceof Bar) {
+//             // dtto for Bar
+//         }
+//     }
+// }
+
+// class Testme {
+//     public function __construct() {
+//         $result = (new Foo())->doSomethingUseful([new Foo()]);
+//     }
+// }
+
+
+
+class noFoo{
+    function __construct(){
+        echo "here";
     }
 }
 
 class Testme {
     public function __construct() {
-        $result = (new Foo())->doSomethingUseful([new Foo()]);
+       echo "doSomethingUseful";
+    }
+
+    public function defined(){
+        return "hey";
+    }
+}
+class FooWho {
+    /**
+     * @param Testme
+     */
+    public function doSomethingUseful(Testme $obj)
+    {
+        if (!$obj instanceof noFoo) {
+            return;
+        } 
+
+        $var = $obj -> defined();;
+        $next = $obj;
+        echo "got it";
+        
     }
 }
 
