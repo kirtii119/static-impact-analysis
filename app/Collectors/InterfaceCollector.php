843817951a7924fa8da3d8f5053ae7c3318657e2
SYBhavsar <bhavsarsuyash30@gmail.com>
2024-03-18 11:07:04 +0530

    added interface collector

---
+++ app/Collectors/InterfaceCollector.php
@@ -0,0 +1,29 @@
+<?php
+namespace App;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Collectors\Collector;
+
+
+class InterfaceCollector implements Collector
+{
+
+    public function getNodeType(): string
+    {
+        return 'PhpParser\Node\Stmt\Class_';
+    }
+
+    public function processNode(Node $node, Scope $scope): array
+    {
+        $temp = $node->implements;
+        $temp2 = $node->implements[0]->parts;
+        if($temp != null){
+            $className = implode("\\", $node->namespacedName->parts);
+            $implements = implode("\\", $temp2);
+            file_put_contents('InterfaceImpl.txt', $implements ." => ".$className.PHP_EOL,FILE_APPEND);
+
+        } 
+        return [];
+    }
+}
